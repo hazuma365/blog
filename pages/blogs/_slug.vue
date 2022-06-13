15b2d2dac29e28a4141130bd764ccda5a10b52c1
hazuma365 <hazuma365@gmail.com>
2022-06-13 22:14:37 +0900

    update

---
+++ pages/blogs/_slug.vue
@@ -0,0 +1,15 @@
+<template>
+ <article>
+   <h1>{{blogs.title}}</h1>
+   <p>{{blogs.date}}</p>
+   <nuxt-content :document="blogs" />
+ </article>
+</template>
+<script>
+export default {
+ async asyncData ({ $content, params }) {
+   const blogs = await $content('blogs', params.slug || 'index').fetch()
+   return { blogs }
+ }
+}
+</script>
---
+++ pages/blogs/index.vue
@@ -0,0 +1,16 @@
+<template>
+ <div>
+   <div v-for="b in blogs" :key="b.slug">
+     <nuxt-link :to="'/blogs/'+ b.slug">{{b.title}} {{b.date}}</nuxt-link>
+   </div>
+ </div>
+</template>
+<script>
+export default {
+ async asyncData ({ $content, params }) {
+   const query = await $content('blogs' || 'index').limit(10)
+   const blogs = await query.fetch()
+   return { blogs }
+ }
+}
+</script>
