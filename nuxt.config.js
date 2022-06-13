6cdcd9cdc827c2fbff2b4da4d7eac34961350e2d
hazuma365 <hazuma365@gmail.com>
2022-06-13 22:14:01 +0900

    update gitignore

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,7 +36,8 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-  ],
+     '@nuxt/content'
+   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
