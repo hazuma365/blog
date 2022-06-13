1b25e79f514c4607db7467e64eccb6622a875487
hazuma365 <hazuma365@gmail.com>
2022-06-13 23:39:17 +0900

    add vuetify.js

--- pages/blogs/index.vue
+++ pages/blogs/index.vue
@@ -1,7 +1,24 @@
 <template>
  <div>
    <div v-for="b in blogs" :key="b.slug">
-     <nuxt-link :to="'/blogs/'+ b.slug">{{b.title}} {{b.date}}</nuxt-link>
+  <v-card
+    class="mx-auto"
+    max-width="500"
+    tile
+  >
+       <v-card-title>
+         <div>{{b.title}}</div>
+       </v-card-title>
+      <v-card-subtitle>
+        <div>{{b.date}}</div>
+      </v-card-subtitle>
+       <v-card-text>
+         <div>本文</div>
+       </v-card-text>
+       <v-card-actions>
+          <nuxt-link :to="'/blogs/'+ b.slug">more</nuxt-link>
+       </v-card-actions>
+     </v-card>
    </div>
  </div>
 </template>
