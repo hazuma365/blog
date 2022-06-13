6afdb47f7ff35eefda5636c97006c5a56f1c6b5a
hazuma365 <91006516+hazuma365@users.noreply.github.com>
2022-06-13 13:53:16 +0000

    Deploying to gh-pages from @ hazuma365/my-nuxt-project@3f1201848f71a27186ebcfed3ac4306659ac9514 🚀

--- _nuxt/static/1655127061/blogs/blog/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmy-nuxt-project\u002F_nuxt\u002Fstatic\u002F1655127061",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblogs\u002Fblog",config:{_app:{basePath:"\u002Fmy-nuxt-project\u002F",assetsPath:"\u002Fmy-nuxt-project\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3b535e6e"}}}}(null));
