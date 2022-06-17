530366db02281bcf72242bab05470d3e3d538b5b
hazuma365 <hazuma365@gmail.com>
2022-06-17 23:37:23 +0900

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,40 @@
 <template>
  <div>
-   <div v-for="b in blogs" :key="b.slug">
-  <v-card
-    class="mx-auto"
-    max-width="500"
-    tile
-  >
-       <v-card-title>
-         <div>{{b.title}}</div>
-       </v-card-title>
-      <v-card-subtitle>
-        <div>{{b.date}}</div>
-      </v-card-subtitle>
-       <v-card-text>
-         <div>{{b.description}}</div>
-       </v-card-text>
-       <v-card-actions>
-          <nuxt-link :to="'/blogs/'+ b.slug">more</nuxt-link>
-       </v-card-actions>
-     </v-card>
-   </div>
+   <v-container>
+     <v-row dense>
+       <v-col
+         v-for="b in blogs" :key="b.slug""
+         cols="12"
+       >
+         <v-card>
+           <div class="d-flex flex-no-wrap justify-space-between">
+             <div>
+               <v-card-title
+                 class="text-h5"
+                 v-text="b.title"
+               ></v-card-title>
+               <v-card-subtitle v-text="b.date"></v-card-subtitle>
+                <v-card-text>
+                  <div>{{b.description}}</div>
+                </v-card-text>
+               <v-card-actions>
+                  <nuxt-link :to="'/blogs/'+ b.slug">more</nuxt-link>
+               </v-card-actions>
+             </div>
+           </div>
+         </v-card>
+       </v-col>
+     </v-row>
+   </v-container>
+  <v-footer absolute>
+    <v-col
+      class="text-center"
+      cols="12"
+    >
+    </v-col>
+  </v-footer>
  </div>
+
 </template>
 <script>
 export default {
