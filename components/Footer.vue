725e8354823fa3ca3e4e01e797070aa442cfd574
hazuma365 <hazuma365@gmail.com>
2022-06-18 21:55:30 +0900

    update

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,10 +1,12 @@
 <template>
   <v-footer absolute>
-    <v-col
-      class="text-center"
-      cols="12"
-    >
-      (c){{ new Date().getFullYear() }} hazuma365
-    </v-col>
+     <v-container>
+        <v-col
+          class="text-center"
+          cols="12"
+        >
+          (c){{ new Date().getFullYear() }} hazuma365
+        </v-col>
+     </v-container>
   </v-footer>
 </template>
