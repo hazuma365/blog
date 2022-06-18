58e8def8abbb7a971551da7d09e28fd8d29117a5
hazuma365 <hazuma365@gmail.com>
2022-06-18 17:47:06 +0900

    add top

---
+++ components/TopBanner.vue
@@ -0,0 +1,7 @@
+<template>
+   <v-container>
+       <v-col cols="12">
+       hazuma365@blog
+       </v-col>
+   </v-container>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
  <div>
    <navbar/>
+   <topBanner/>
    <v-container>
      <v-row dense>
        <v-col
