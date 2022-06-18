f8240657bb9844811d86afc8b618b6391612adaf
hazuma365 <hazuma365@gmail.com>
2022-06-18 20:32:05 +0900

    update

--- pages/articles/_slug.vue
+++ pages/articles/_slug.vue
@@ -1,10 +1,22 @@
 <template>
-   <article>
-     <h1>{{article.title}}</h1>
-     <p>formatDate(article.date)</p>
-     <nuxt-content :document="article" />
-     <prev-next :prev="prev" :next="next" />
-   </article>
+   <v-container>
+     <v-row dense>
+        <v-col cols="12">
+        <v-card>
+           <article>
+               <v-card-title
+                 class="text-h1"
+                 v-text="article.title"
+               ></v-card-title>
+               <v-card-subtitle v-text=formatDate(article.updatedAt)></v-card-subtitle>
+             <v-card-text>
+                <nuxt-content :document="article" />
+             </v-card-text>
+           </article>
+        </v-card>
+        </v-col>
+     </v-row>
+  </v-container>
 </template>
 <script>
 export default {
@@ -21,27 +33,38 @@ export default {
         next
       }
     },
+   methods: {
+       formatDate(date) {
+         const options = { year: 'numeric', month: 'long', day: 'numeric' }
+         return new Date(date).toLocaleDateString('jp', options)
+       }
+    }
 }
 </script>
 
 <style>
+  .nuxt-content h1 {
+    font-weight: bold;
+    font-size: 30px;
+    line-height: 2.0em;
+  }
   .nuxt-content h2 {
     font-weight: bold;
+    font-size: 25px;
+    line-height: 1.5em;
+  }
+  .nuxt-content h3 {
+    font-weight: bold;
     font-size: 20px;
-  }
-  .nuxt-content h3 {
-    font-weight: bold;
-    font-size: 22px;
-  }
-  .nuxt-content h3 {
-    font-weight: bold;
-    font-size: 22px;
+    line-height: 1.5em;
   }
   .nuxt-content h4 {
     font-weight: bold;
-    font-size: 22px;
+    font-size: 20px;
+    line-height: 1.5em;
   }
   .nuxt-content p {
     margin-bottom: 20px;
+    line-height: 1.5em;
   }
 </style>
