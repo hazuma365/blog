81696063b82aa5a4f5cffac236fd4abc7e278893
hazuma365 <91006516+hazuma365@users.noreply.github.com>
2022-06-19 04:16:33 +0000

    Deploying to gh-pages from @ hazuma365/my-nuxt-project@31d762e056913b2f42ba5e88fe6d679bc7698377 🚀

--- _nuxt/static/1655606528/articles/article1/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmy-nuxt-project\u002F_nuxt\u002Fstatic\u002F1655606528",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Farticle1",config:{_app:{basePath:"\u002Fmy-nuxt-project\u002F",assetsPath:"\u002Fmy-nuxt-project\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2a551080"}}}}(null));
--- _nuxt/static/1655606528/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Farticles\u002Farticle5","\u002Farticles\u002Farticle4","\u002Farticles\u002Farticle3","\u002Farticles\u002Farticle1","\u002Farticles\u002Farticle2"]})
