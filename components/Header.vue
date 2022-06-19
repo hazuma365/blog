6bc0b8433a53dc77bda40a01b2ceddebd7f801cc
hazuma365 <hazuma365@gmail.com>
2022-06-19 15:50:45 +0900

    update

--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,9 @@
 <template>
-      <v-container>
+      <v-container style="max-width: 645px;">
        <v-col
         cols="12"
         align="center">
-        <v-row style="width: 645px;">
+        <v-row>
         <button v-on:click="home">hazuma365@blog</button>
         </v-row>
        </v-col>
