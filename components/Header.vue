4ed44af97cddfdb8a6933e213b7c21b172181256
hazuma365 <hazuma365@gmail.com>
2022-06-19 13:35:00 +0900

    top link center

--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,11 @@
 <template>
-      <v-container >
-        <v-row >
-           <v-col>
+      <v-container>
+       <v-col
+        cols="12"
+        align="center">
+        <v-row style="width: 645px;">
             <nuxt-link to="/">hazuma365@blog</nuxt-link>
-           </v-col>
         </v-row>
+       </v-col>
      </v-container>
 </template>
