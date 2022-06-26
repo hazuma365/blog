f5c8c7a6c7da0b05ea0532c6ce5b56c0cff1732e
hazuma365 <hazuma365@gmail.com>
2022-06-26 15:23:27 +0900

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,38 @@
 <template>
- <div>
-   <v-container>
-     <v-row>
-       <v-col
-         v-for="article in articles" :key="article.slug""
-         cols="12"
-         align="center">
-         <v-card width="645">
-               <v-card-title
-                 class="text-h5"
-                 v-text="article.title"
-               ></v-card-title>
-               <v-card-subtitle>
-                  <div style="text-align: left">
-                    {{formatDate(article.updatedAt)}}
-                  </div>
-               </v-card-subtitle>
-               <v-card-actions>
-                  <nuxt-link :to="'/articles/'+ article.slug">read</nuxt-link>
-               </v-card-actions>
-         </v-card>
-       </v-col>
-     </v-row>
-   </v-container>
- </div>
-
+  <div>
+    <v-container>
+      <v-row>
+        <v-col
+          v-for="article in articles" :key="article.slug""
+          cols="12"
+          align="center">
+          <v-card width="645">
+                <v-card-title
+                  class="text-h5"
+                  v-text="article.title"
+                >
+                </v-card-title>
+                <v-card-subtitle>
+                    <div style="text-align: left">
+                      {{formatDate(article.updatedAt)}}
+                    </div>
+                </v-card-subtitle>
+                <v-card-actions class="test">
+                    <div style="text-align: right">
+                      <nuxt-link :to="'/articles/'+ article.slug">read ...</nuxt-link>
+                    </div>
+                </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 <script>
 export default {
    async asyncData ({ $content, params }) {
      const query = await $content('articles' || 'index')
-     const articles = await query.sortBy('date', 'desc').fetch()
+     const articles = await query.where({ status: 'public' }).sortBy('date', 'desc').fetch()
      return { articles }
    },
   methods: {
@@ -41,3 +43,10 @@ export default {
    }
 }
 </script>
+<style>
+a {
+  color: #6666CC;
+  text-decoration: none;
+  text-align: right;
+}
+</style>
